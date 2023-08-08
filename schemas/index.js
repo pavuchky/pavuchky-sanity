a8c4d2aba01d55ce64e740be4046696fb5ef8c8d
Vladyslav <vladyslav@MacBook-Air-Vladyslav.local>
2023-08-08 18:31:55 +0300

    connect schemas

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,15 @@
-export const schemaTypes = []
+import contactsSchema from './documents/contacts'
+import partnersSchema from './documents/partners'
+import postsSchema from './documents/posts'
+import reviewsSchema from './documents/reviews'
+import statisticSchema from './documents/statistic'
+// import localizedString from './objects/localizedString'
+
+export const schemaTypes = [
+  contactsSchema,
+  partnersSchema,
+  postsSchema,
+  reviewsSchema,
+  statisticSchema,
+  //   localizedString
+]
